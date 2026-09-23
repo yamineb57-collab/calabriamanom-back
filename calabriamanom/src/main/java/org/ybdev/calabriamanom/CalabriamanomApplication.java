f753532977f4dc00b2731ba62d43d29a34362e66
yamineb57 <yamineb57@gmail.com>
2026-09-23 20:50:01 +0200

    init spring

---
+++ calabriamanom/src/main/java/org/ybdev/calabriamanom/CalabriamanomApplication.java
@@ -0,0 +1,13 @@
+package org.ybdev.calabriamanom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalabriamanomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalabriamanomApplication.class, args);
+    }
+
+}
---
+++ calabriamanom/src/test/java/org/ybdev/calabriamanom/CalabriamanomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ybdev.calabriamanom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalabriamanomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
